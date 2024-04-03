7cf0f911b8a77ae72b9e9aa4fe68f021781d1323
‘Inna’ <khagleeva_inna@mail.ru>
2024-04-03 18:05:39 +0300

    Каркас главной страницы социальной сети

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="src/logo.svg" />
+    </header>
+  );
+};
+
+export default Header;
