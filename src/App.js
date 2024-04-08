745584e0a6485e137a3c7d2245935983376ef129
‘Inna’ <khagleeva_inna@mail.ru>
2024-04-08 17:30:25 +0300

    Использую старую версию контекста

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ const App = ({ state, dispatch, store }) => {
       <Navbar />
       <div className="app-wrapper-content">
         <Routes>
-          <Route path="/profile" element=<Profile store={store} /> />
-          <Route path="/dialogs/*" element=<DialogsContainer store={store} /> />
+          <Route path="/profile" element=<Profile /> />
+          <Route path="/dialogs/*" element=<DialogsContainer /> />
           <Route path="/news" element=<News /> />
           <Route path="/music" element=<Music /> />
           <Route path="/settings" element=<Settings /> />
--- src/components/Dialogs/DialogsContainer.js
+++ src/components/Dialogs/DialogsContainer.js
@@ -4,26 +4,32 @@ import {
   sendMessageActionCreator,
   changeNewMessageTextActionCreator,
 } from "../../redux/dialogs-reducer";
+import StoreContext from "../../StoreContext";
 
-const DialogsContainer = ({ store }) => {
-  const handleClick = () => {
-    store.dispatch(sendMessageActionCreator());
-    store.dispatch(changeNewMessageTextActionCreator(""));
-  };
-
-  const handleChange = (event) => {
-    let messageText = event.target.value;
-    store.dispatch(changeNewMessageTextActionCreator(messageText));
-  };
-
+const DialogsContainer = () => {
   return (
-    <Dialogs
-      dialogsData={store.getState().dialogsReducer.dialogsData}
-      messages={store.getState().dialogsReducer.messages}
-      onChange={handleChange}
-      onClick={handleClick}
-      newMessageText={store.getState().dialogsReducer.newMessageText}
-    />
+    <StoreContext.Consumer>
+      {(store) => {
+        const handleClick = () => {
+          store.dispatch(sendMessageActionCreator());
+          store.dispatch(changeNewMessageTextActionCreator(""));
+        };
+
+        const handleChange = (event) => {
+          let messageText = event.target.value;
+          store.dispatch(changeNewMessageTextActionCreator(messageText));
+        };
+        return (
+          <Dialogs
+            dialogsData={store.getState().dialogsReducer.dialogsData}
+            messages={store.getState().dialogsReducer.messages}
+            onChange={handleChange}
+            onClick={handleClick}
+            newMessageText={store.getState().dialogsReducer.newMessageText}
+          />
+        );
+      }}
+    </StoreContext.Consumer>
   );
 };
 
