0f42c3e00eda53c31ebf809e9665270d82dbc4e7
‘Inna’ <khagleeva_inna@mail.ru>
2024-04-09 16:27:12 +0300

    Написала классовую компоненту

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,5 +1,5 @@
 import { connect } from "react-redux";
-import Users from "./UsersC";
+import Users from "./Users";
 import { followAC, unfollowAC, setUsersAC } from "../../redux/users-reducer";
 
 const mapStateToProps = (state) => ({
