74cb33b6bf014614126bc2f8b7e338325ab58615
‘Inna’ <khagleeva_inna@mail.ru>
2024-04-09 16:35:52 +0300

    Перенесла ajax-запрос в конструктор

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -5,19 +5,18 @@ import avatar from "../../assets/avatar.jpeg";
 const URL = "https://social-network.samuraijs.com/api/1.0/users";
 
 class Users extends React.Component {
-  getUsersFoo = () => {
-    if (this.props.users.length === 0) {
-      axios.get(URL).then((response) => {
-        this.props.setUsers(response.data.items);
-      });
-    }
-  };
+  constructor(props) {
+    super(props);
+
+    axios.get(URL).then((response) => {
+      this.props.setUsers(response.data.items);
+    });
+  }
 
   //метод render вернет jsx
   render() {
     return (
       <>
-        <button onClick={this.getUsersFoo}>получить пользователей</button>
         {this.props.users.map((user) => (
           <div key={user?.id} className={styles.user}>
             <div>
